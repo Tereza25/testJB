a4eb0397ab121a1868af111ab78c6c06c6817da0
Tereza25 <rh.cris.ead@gmail.com>
2022-08-18 00:48:02 -0300

    Atualizando bibliotecas e arquitetura MCR

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const {
-  create, update, remove, show,
-} = require('../controllers/productController');
-
-// Criar rotas para o CRUD de produto
-
-module.exports = router;
